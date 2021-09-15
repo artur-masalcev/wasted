cae6ff301ad655389cc2bd14ebebaa0de15229d5
artur-masalcev <arturmasalcev@gmail.com>
2021-09-15 16:50:09 +0300

    Implement dummy data provider
    Please note that I've moved dummy data folder into the project root so app could see it.
    
    Tested only on iOS 14.5 simulator, so, perhaps, additional Android testing is needed

---
+++ Wasted/Utils/EmbeddedDataReader.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace Wasted.Utils
+{
+    public class EmbeddedDataReader
+    {
+        /// <summary>
+        /// Reads the data from embedded data in the app
+        /// </summary>
+        /// <param name="path">Path to the data</param>
+        /// <returns>String containing the embedded data</returns>
+        /// <exception cref="System.ArgumentNullException">Thrown when there is not data in the given path</exception>
+        public static string ReadString(string path)
+        {
+            var assembly = IntrospectionExtensions.GetTypeInfo(typeof(MainPage)).Assembly;
+            Stream stream = assembly.GetManifestResourceStream(path);
+            string text = "";
+
+            using (var reader = new StreamReader(stream))
+            {
+                text = reader.ReadToEnd();
+            }
+
+            return text;
+        }
+    }
+}
