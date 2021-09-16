a13f2685f36b12f874318af8d371d5686c9ce805
artur-masalcev <arturmasalcev@gmail.com>
2021-09-16 18:51:58 +0300

    Add business objects
    Added business objects for containing converted JSON data from dummy API. I decided not to include convertation function as Newtonsoft JSON library does it all in one line of code. For example here is how you get all the deals
    string dealsJSON = DummyDataAPI.DummyDataProvider.GetDeals();
                DummyAPI.BusinessObjects.Deal[] deals = JsonConvert.DeserializeObject<DummyAPI.BusinessObjects.Deal[]>(dealsJSON);
    
    Tested on iOS 14.5 simulator

---
+++ Wasted/Dummy API/Business Objects/Deal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Wasted.DummyAPI.BusinessObjects
+{
+    public class Deal
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public double CurrentCost { get; set; }
+        public double PreviousCost { get; set; }
+        public int Quantity { get; set; }
+        public string Due { get; set; }
+        public string ImageURL { get; set; }
+
+        public Deal(int id, string title, double currentCost, double previousCost, int quantity, string due, string imageURL)
+        {
+            ID = id;
+            Title = title;
+            CurrentCost = currentCost;
+            PreviousCost = previousCost;
+            Quantity = quantity;
+            Due = due;
+            ImageURL = imageURL;
+        }
+    }
+}
