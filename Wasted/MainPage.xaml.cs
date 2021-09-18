195c15f564b9cd582447224e4af9974ef6837a1e
Mantas Pilipuitis <mantas.pilipuitis@gmail.com>
2021-09-18 22:37:21 +0300

    Add bigger resolution pictures. Comment out unecessary tabs

--- Wasted/MainPage.xaml.cs
+++ Wasted/MainPage.xaml.cs
@@ -16,13 +16,16 @@ namespace Wasted
             InitializeImageIcons();
         }
 
+        /// <summary>
+        /// Adds icons to navigation bar. Orders page and profile page are excluded until MVP will be done.
+        /// </summary>
         public void InitializeImageIcons()
         {
             var assembly = typeof(MainPage); //Finds if program runs on android or on ios and adjust images accordingly.
-            homePage.IconImageSource = ImageSource.FromResource("Wasted.Images.Icons.outline_home_black_24dp.png", assembly);
-            searchPage.IconImageSource = ImageSource.FromResource("Wasted.Images.Icons.outline_search_black_24dp.png", assembly);
-            ordersPage.IconImageSource = ImageSource.FromResource("Wasted.Images.Icons.outline_receipt_long_black_24dp.png", assembly);
-            profilePage.IconImageSource = ImageSource.FromResource("Wasted.Images.Icons.outline_person_black_24dp.png", assembly);
+            homePage.IconImageSource = ImageSource.FromResource("Wasted.Images.Icons.outline_home_black_48dp.png", assembly);
+            searchPage.IconImageSource = ImageSource.FromResource("Wasted.Images.Icons.outline_search_black_48dp.png", assembly);
+            //ordersPage.IconImageSource = ImageSource.FromResource("Wasted.Images.Icons.outline_receipt_long_black_48dp.png", assembly);
+            //profilePage.IconImageSource = ImageSource.FromResource("Wasted.Images.Icons.outline_person_black_48dp.png", assembly);
         }
 
 
