cc88908f8b26489e98bf24c21a8fe0b601b2fc07
artur-masalcev <arturmasalcev@gmail.com>
2021-09-20 21:22:47 +0300

    Make navigation bar icons native and adaptive

--- Wasted/MainPage.xaml.cs
+++ Wasted/MainPage.xaml.cs
@@ -3,26 +3,14 @@
 namespace Wasted
 {
     /// <summary>
-    /// Core screen of the app, manages transitioning between other screens
+    /// Core page of the app, manages transitioning between other screens
     /// </summary>
     public partial class MainPage : TabbedPage
     {
         public MainPage()
         {
             InitializeComponent();
-            InitializeImageIcons();
         }
-
-        /// <summary>
-        /// Adds icons to navigation bar.
-        /// </summary>
-        public void InitializeImageIcons()
-        {
-            var assembly = typeof(MainPage); //Find if program runs on android or on ios and adjust images accordingly
-            homePage.IconImageSource = ImageSource.FromResource("Wasted.Images.Icons.outline_home_black_48dp.png", assembly);
-            searchPage.IconImageSource = ImageSource.FromResource("Wasted.Images.Icons.outline_search_black_48dp.png", assembly);
-        }
-
 
     }
 }
