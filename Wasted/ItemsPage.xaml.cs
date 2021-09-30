143709cd26b0733da19df4fc807059868c17f1cd
artur-masalcev <arturmasalcev@gmail.com>
2021-09-30 19:03:24 +0300

    Fix and refactor deal page

--- Wasted/ItemsPage.xaml.cs
+++ Wasted/ItemsPage.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Wasted.DummyAPI.BusinessObjects;
+﻿using Wasted.DummyAPI.BusinessObjects;
 using Xamarin.Forms;
 using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 using Xamarin.Forms.Xaml;
