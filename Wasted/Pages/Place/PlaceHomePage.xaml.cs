4c4f91f3895fc7caf235ffcb190fc946703a2d60
Vilius Junevicius <viliusjun@gmail.com>
2021-11-07 20:53:51 +0200

    Delete FoodPlace from OwnedPlaces

--- Wasted/Pages/Place/PlaceHomePage.xaml.cs
+++ Wasted/Pages/Place/PlaceHomePage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Wasted.Dummy_API.Business_Objects;
 using Wasted.DummyAPI.BusinessObjects;
 using Wasted.Pages.Place.NewDeal;
@@ -18,12 +19,16 @@ namespace Wasted.Pages.Place
         private DataService service;
         public UserPlace CurrentUser { get; set; }
         public IEnumerable<FoodPlace> OwnedPlaces { get; set; }
+        public ICommand DeleteCommand { get; }
+        public Button DeleteButton;
         public PlaceHomePage()
         {
             service = DependencyService.Get<DataService>();
             CurrentUser = (UserPlace)service.CurrentUser;
             BindingContext = this;
             OwnedPlaces = CurrentUser.OwnedPlaceIDs.Select(id => service.AllFoodPlaces[id - 1]); // Selects appropriate food place based on index
+            DeleteCommand = new Command(DeletePlace);
+            DeleteButton.BindingContext = DeleteCommand;
             InitializeComponent();
         }
 
@@ -39,6 +44,13 @@ namespace Wasted.Pages.Place
         private void AddNewPlaceButtonClicked(object sender, EventArgs e)
         {
             Navigation.PushAsync(new NewPlacePage());
+        }
+
+        private void DeletePlace(object obj)
+        {
+            var foodPlace = obj as FoodPlace;
+            OwnedPlaces.ToList().Remove(foodPlace);
+            Console.WriteLine("removed");
         }
     }
 }
