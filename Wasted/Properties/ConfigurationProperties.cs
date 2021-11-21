b169f295d4e9aec9378d6a57aac65049dba9d9d7
Mantas Pilipuitis <mantas.pilipuitis@gmail.com>
2021-11-21 09:45:43 +0200

    Add unwanted removed files

---
+++ Wasted/Properties/ConfigurationProperties.cs
@@ -0,0 +1,11 @@
+﻿namespace Wasted.Properties
+{
+    public static class ConfigurationProperties
+    {
+        public const string LocalIPAddress = "192.168.39.188";
+
+        // Run cmd
+        // ipconfig
+        // Find IPv4 Address
+    }
+}
