5d86599dc55bf88e9e1641b08bfcd299596442bb
artur-masalcev <arturmasalcev@gmail.com>
2021-11-21 17:33:27 +0200

    Add settings page layout. Change navigation bar icons

---
+++ Wasted/Pages/Place/SettingsPage.xaml.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+using Xamarin.Forms.Xaml;
+
+namespace Wasted.Pages.Place
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SettingsPage : ContentPage
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+            On<iOS>().SetUseSafeArea(true);
+        }
+    }
+}
