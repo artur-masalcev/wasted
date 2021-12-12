8d0d681f4f96261433f46648bb25d4e06fc4f5a5
artur-masalcev <arturmasalcev@gmail.com>
2021-12-12 02:34:27 +0200

    Add settings page. Readjust icons

---
+++ Wasted/Pages/Place/SettingsPage.xaml.cs
@@ -0,0 +1,17 @@
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+using Xamarin.Forms.Xaml;
+
+namespace Wasted.Pages.Place
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SettingsPage : ContentPage
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+            On<iOS>().SetUseSafeArea(true);
+        }
+    }
+}
