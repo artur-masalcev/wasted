e5a5bf75d801b55d4b46e4d6bb7aefc24deac86b
artur-masalcev <arturmasalcev@gmail.com>
2021-12-14 02:06:53 +0200

    Refactor Wasted unit tests. Add new tests. Make deal equatable

--- Wasted.UnitTests/AppInitializer.cs
+++ Wasted.UnitTests/AppInitializer.cs
@@ -2,7 +2,7 @@
 using Xamarin.UITest;
 using Xamarin.UITest.Queries;
 
-namespace WastedTest
+namespace Wasted.UnitTests
 {
     public class AppInitializer
     {
---
+++ Wasted.UnitTests/ExceptionCheckerTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+using Wasted.Utils.Exceptions;
+
+namespace Wasted.UnitTests
+{
+    public class ExceptionCheckerTest
+    {
+        [Test]
+        public void CheckValidParams_ShouldThrowException_WhenThereIsEmptyString()
+        {
+            // Arrange
+            string[] testStringArray = {"", "hello", " world"};
+
+            // Act
+
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => ExceptionChecker.CheckValidParams(testStringArray));
+        }
+
+        [Test]
+        public void CheckValidParams_ShouldNotThrowAnyException_WhenAllStringsAreNotNullAndNotEmpty()
+        {
+            // Arrange
+            string[] testStringArray = {"hello", "world", "!"};
+
+            // Act
+
+            // Assert
+            Assert.DoesNotThrow(() => ExceptionChecker.CheckValidParams(testStringArray));
+        }
+
+        [Test]
+        public void CheckValidParams_ShouldThrowException_WhenThereIsNullString()
+        {
+            // Arrange
+            string[] testStringArray = {"hello", null, " world"};
+
+            // Act
+
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => ExceptionChecker.CheckValidParams(testStringArray));
+        }
+    }
+}
